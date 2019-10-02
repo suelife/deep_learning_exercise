e5018baf8060cf41f4f1b67d7d032a67b63e7271
wind <suelife@gmail.com>
2019-10-02 16:15:20 +0800

    add softmax

---
+++ exercise/ch03/softmax.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def softmax(x):
+    x_max = np.max(x)
+    x_exp = np.exp(x - x_max)
+    x_sum_exp = np.sum(x_exp)
+
+    return x_exp / x_sum_exp
+
+if __name__ == '__main__':
+    x = np.array([0.3, 2.9, 4.0])
+    y = softmax(x)
+    print(y)
