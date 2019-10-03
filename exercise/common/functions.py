88927260edf613e6ddc2744f5d48180cb64ed8eb
wind <suelife@gmail.com>
2019-10-03 13:09:33 +0800

    Add ReLU

--- exercise/common/functions.py
+++ exercise/common/functions.py
@@ -11,19 +11,27 @@ def sigmoid(x, deriv=False):
     else:
         return 1 / (1 + np.exp(-x))
 
+def relu(x):
+    return np.maximum(0, x)
+
 
 if __name__ == '__main__':
     x = np.arange(-5.0, 5.0, 0.1)
     step_y = step_function(x)
     sig_y = sigmoid(x)
+    relu_y = relu(x)
     
-    plt.subplot(1, 2, 1)
+    plt.subplot(1, 3, 1, figsize=(15,15))
     plt.plot(x, step_y)
-    plt.title('step_function')
+    plt.title('Step Function')
 
-    plt.subplot(1, 2, 2)
+    plt.subplot(1, 3, 2)
     plt.plot(x, sig_y)
-    plt.title('sigmoid')
+    plt.title('Sigmoid')
+
+    plt.subplot(1, 3, 3)
+    plt.plot(x, relu_y)
+    plt.title('ReLU')
 
     plt.show()
     
