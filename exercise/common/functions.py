af78d074fe04ed94c1d0f48d073a882bf901a0d6
wind <suelife@gmail.com>
2019-10-05 10:43:12 +0800

    Add comment and change functions name

--- exercise/common/functions.py
+++ exercise/common/functions.py
@@ -28,30 +28,35 @@ def softmax(x):
     total_exp = np.sum(exp_x)
     return exp_x / total_exp
 
-def mean_squared(y, t):
+# ch04
+def mean_squared_error(y, t):
     return 0.5 * np.sum((y - t)**2)
 
-def cross_entropy(y, t):
+def cross_entropy_error(y, t):
     delta = 1e-7
     return -np.sum(t*np.log(y + delta)) # Add a small value to prevent negative infinity
 
 if __name__ == '__main__':
-    x = np.arange(-5.0, 5.0, 0.1)
-    step_y = step_function(x)
-    sig_y = sigmoid(x)
-    relu_y = relu(x)
+    # x = np.arange(-5.0, 5.0, 0.1)
+    # step_y = step_function(x)
+    # sig_y = sigmoid(x)
+    # relu_y = relu(x)
     
-    plt.subplot(1, 3, 1, figsize=(15,15))
-    plt.plot(x, step_y)
-    plt.title('Step Function')
+    # plt.subplot(1, 3, 1, figsize=(15,15))
+    # plt.plot(x, step_y)
+    # plt.title('Step Function')
 
-    plt.subplot(1, 3, 2)
-    plt.plot(x, sig_y)
-    plt.title('Sigmoid')
+    # plt.subplot(1, 3, 2)
+    # plt.plot(x, sig_y)
+    # plt.title('Sigmoid')
 
-    plt.subplot(1, 3, 3)
-    plt.plot(x, relu_y)
-    plt.title('ReLU')
+    # plt.subplot(1, 3, 3)
+    # plt.plot(x, relu_y)
+    # plt.title('ReLU')
 
-    plt.show()
-    
+    # plt.show()
+
+    # t = np.array([0, 0, 1, 0, 0, 0, 0,  0, 0, 0])
+    # y = np.array([0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0])
+    # resu = mean_squared_error(y, t)
+    # print(resu)
