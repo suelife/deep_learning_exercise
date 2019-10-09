e9da2137af4083899f13af8a453ce29eaea14bc1
wind <suelife@gmail.com>
2019-10-09 12:37:13 +0800

    change code

--- exercise/ch04/gradient_1d.py
+++ exercise/ch04/gradient_1d.py
@@ -1,8 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def numerical_diff(f, x):
-    h = 1e-04
+    '''
+    微分中的h趨近於0，但數值微分中的h並非如此
+    因此數值微分所求解為(x+h)與x之間的斜率，而非x的斜率
+    為了改善誤差所造成的情況，計算(x+h)與(x-h)的函數f差分
+    藉此減少之間的誤差
+    '''
+    h = 1e-4
     return (f(x+h) - f(x-h)) / (2*h)
 
 def function_1(x):
@@ -11,22 +18,21 @@ def function_1(x):
 def tangent_line(f, x):
     d = numerical_diff(f, x)
     print(d)
+    y_f = f(x)
+    d_x = d*x
+    print(y_f)
+    print(d_x)
     y = f(x) - d*x
-
-    print(lambda t: d*t + y)
-
     return lambda t: d*t + y
 
 x = np.arange(0.0, 20.0, 0.1)
-y = function_1(x)
+y1 = function_1(x)
+plt.plot(x, y1)
+plt.xlabel('x')
+plt.ylabel('f(x)')
 
 tf = tangent_line(function_1, 5)
-print(tf)
 y2 = tf(x)
+plt.plot(x, y2)
 
-# plt.plot(x, y)
-# plt.plot(x, y2)
-# plt.xlabel('x')
-# plt.ylabel('f(x)')
-
-# plt.show()
+plt.show()
