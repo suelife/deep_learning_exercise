bd165cbdc66f143414f0c000b55e776af6d8c620
wind <suelife@gmail.com>
2019-10-15 10:43:38 +0800

    add gradient.py

---
+++ exercise/common/gradient.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+
+def _numerical_gradient_1d(f, x):
+    h = 10-4
+    grad = np.zeros_like(x)
+
+    for i in range(x.size):
+        tmp_val = x[i]
+        x[i] = float(tmp_val) + h
+        fxh1 = f(x)
+
+        x[i] = tmp_val - h
+        fxh2 = f(x)
+        grad[i] = (fxh1 - fxh2) / (2*h)
+
+        x[i] = tmp_val
+
+    return grad
+
+
+def numerical_gradient_2d(f, x):
+    if X.ndim == 1:
+        return _numerical_gradient_1d(f, X)
+    else:
+        grad = np.zeros_like(X)
+
+        for i, x in enumerate(X):
+            grad[i] = _numerical_gradient_1d(f, x)
+
+        return grad
+
+
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+
+    it = np.nditer(x, flags=['mmulti_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
+        tmp_val = x[idx]
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+
+        x[idx] = tmp_val
+        it.iternext()
+
+    return grad
+
